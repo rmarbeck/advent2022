e3c8d751a5376d7d56b35cdd8daa8dfa12c45674
Marbeck <raphael@watchnext.fr>
2023-12-02 09:55:03 +0100

    fully functional

--- 1-12-1/src/main/scala/Main.scala
+++ 1-12-1/src/main/scala/Main.scala
@@ -1,28 +1,15 @@
 import scala.io.Source
 import scala.math._
 
-import scala.collection.mutable
-
-
 @main def hello: Unit =
   println("Launching 1-12-1")
   val bufferedSource = Source.fromFile("./src/main/resources/test1.txt")
-  var totals: Seq[Int] = Seq()
-  var current = 0
-  for (line <- bufferedSource.getLines)
-    if (line.isEmpty) {
-      totals = current+:totals
-      current = 0
-    } else {
-      current = current+line.toInt
-    }
-  val result = totals.sorted.reverse.take(3).sum
-  println(s"Result is ${result}")
+  val result = bufferedSource.getLines().map(value =>
+    value match
+      case "" => ";0"
+      case number => number
+  ).mkString("+").split(";").map(_.split('+').map(_.toInt).sum).sorted.reverse.take(3)
+  println(s"Result 1 is ${result.head}")
+  println(s"Result 2 is ${result.sum}")
   bufferedSource.close
   println("Done")
-
-def extract(lineContent: String): Int =
-  def first(lineContent: String): Int =
-    lineContent.find(_.isDigit).map(_.asDigit).getOrElse(0)
-  first(lineContent)*10+first(lineContent.reverse)
-
