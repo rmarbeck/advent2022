3829c2d0c0ff1cbede9b1e21c8bf0f9b497f8ace
rmarbeck <rafoufou>
2023-12-04 17:23:57 +0100

    adding 6-12

---
+++ 6-12/src/test/scala/MySuite.scala
@@ -0,0 +1,12 @@
+// For more information on writing tests, see
+// https://scalameta.org/munit/docs/getting-started.html
+class MySuite extends munit.FunSuite:
+  test("main test on final data"):
+    val (score1, score2) = Solver.solve
+    assertEquals(score1, "1802")
+    assertEquals(score2, "3551")
+
+  test("main test on initial data"):
+    val (score1, score2) = Solver.solveTest
+    assertEquals(score1, "7-5-6-10-11")
+    assertEquals(score2, "19-23-23-29-26")
