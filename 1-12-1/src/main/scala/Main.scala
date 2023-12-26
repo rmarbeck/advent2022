fa769fa4f13559075d2b4cdee62a8c8d909f28fa
Marbeck <raphael@watchnext.fr>
2023-12-26 19:41:21 +0100

    1-12 updated

--- 1-12-1/src/main/scala/Main.scala
+++ 1-12-1/src/main/scala/Main.scala
@@ -4,11 +4,13 @@ import scala.math._
 @main def hello: Unit =
   println("Launching 1-12-1")
   val bufferedSource = Source.fromFile("./src/main/resources/test1.txt")
-  val result = bufferedSource.getLines().map(value =>
-    value match
-      case "" => ";0"
-      case number => number
-  ).mkString("+").split(";").map(_.split('+').map(_.toInt).sum).sorted.reverse.take(3)
+  val lines = bufferedSource.getLines().toList
+  given Ordering[Int] = Ordering.Int.reverse
+  val result = lines.foldLeft(List[Int](0)) { (acc, newValue) =>
+    newValue match
+      case "" => 0 +: acc
+      case value => (acc.head + value.toInt) +: acc.tail
+  }.sorted.take(3)
   println(s"Result 1 is ${result.head}")
   println(s"Result 2 is ${result.sum}")
   bufferedSource.close
