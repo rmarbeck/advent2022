f8477defac37f8436e4c384940b1b50cf3355e98
Marbeck <raphael@watchnext.fr>
2023-12-27 14:20:41 +0100

    2-12 updated

--- 2-12/src/main/scala/Main.scala
+++ 2-12/src/main/scala/Main.scala
@@ -1,41 +1,93 @@
 import scala.io.Source
 import scala.math._
+import java.time.Duration
+import java.time.Instant
 
 @main def hello: Unit =
-  println("Launching 1-12-1")
-  val bufferedSource = Source.fromFile("./src/main/resources/test1.txt")
-  val result = bufferedSource.getLines.map(calculateScore(_)).sum
-  println(s"Result is ${result}")
-  bufferedSource.close
-  println("Done")
+  val startTime = Instant.now()
 
-def calculateScore(played: String): Int =
-  def score2Is(first: Char, second: Char): Int =
-    first match
-      case 'A' => second match // Pierre
-        case 'X' => 3 // Ciseaux 0 + 3
-        case 'Y' => 4 // Pierre 3 + 1
-        case _ => 8 // Feuille 6 + 2
-      case 'B' => second match // Feuille
-        case 'X' => 1 // Pierre 0 + 1
-        case 'Y' => 5 // Feuille 3 + 2
-        case _ => 9 // Ciseaux 6 + 3
-      case _ => second match // Ciseaux
-        case 'X' => 2 // Feuille 0 + 2
-        case 'Y' => 6 // Ciseaux 3 + 3
-        case _ => 7 // Pierre 6 + 1
-  def score1Is(first: Char, second: Char): Int =
-    first match
-      case 'A' => second match // Pierre
-        case 'X' => 4 // Pierre 1 + 3
-        case 'Y' => 8 // Feuille 2 + 6
-        case _ => 3  // Ciseaux 3 + 0
-      case 'B' => second match // Feuille
-        case 'X' => 1 // Pierre 1 + 0
-        case 'Y' => 5 // Feuille 2 + 3
-        case _ => 9 // Ciseaux 3 + 6
-      case _ => second match // Ciseaux
-        case 'X' => 7 // Pierre 1 + 6
-        case 'Y' => 2 // Feuille 2 + 0
-        case _ => 6 // Ciseaux 3 + 3
-  score2Is(played.head, played.reverse.head)
+  List[() => (String, String)]( () => Solver.solveTest, () => Solver.solve).foreach: f =>
+    val (score1, score2) = f.apply()
+    println(s"1 : ${score1}")
+    println(s"2 : ${score2}")
+    println(s"----------------")
+  println("Done")
+  println(s"Computing time is ${Duration.between(startTime, Instant.now()).toMillis}ms")
+
+object Solver:
+  def solveTest: (String, String) =
+    solver("test.txt")
+  def solve: (String, String) =
+    solver("data.txt")
+  private def solver(fileName: String): (String, String) =
+    val bufferedSource = Source.fromFile("./src/main/resources/" + fileName)
+    val lines = bufferedSource.getLines().toList
+    bufferedSource.close
+    val (result1, result2) = lines.map(calc).foldLeft((0, 0)):
+      case (acc, (fromOne, fromTwo)) => (acc._1 + fromOne, acc._2 + fromTwo)
+
+    (s"${result1}", s"${result2}")
+
+
+enum PlayResult(val score: Int):
+  case Win extends PlayResult(6)
+  case Draw extends PlayResult(3)
+  case Loss extends PlayResult(0)
+
+enum RPS(val score: Int):
+  case Rock extends RPS(1)
+  case Paper extends RPS(2)
+  case Scissors extends RPS(3)
+
+object RPS:
+  def fromChar(char: Char): RPS =
+    char match
+      case 'A' | 'X' => Rock
+      case 'B' | 'Y' => Paper
+      case 'C' | 'Z' => Scissors
+  def playPart1(rps1: RPS, rps2: RPS): Int =
+    import PlayResult._
+    def matchEnding: PlayResult =
+      (rps1, rps2) match
+        case values @ ((Rock, Paper) | (Paper, Rock)) => values._1 match
+          case Rock => Win
+          case _ => Loss
+        case values @ ((Rock, Scissors) | (Scissors, Rock)) => values._1 match
+          case Scissors => Win
+          case _ => Loss
+        case values @ ((Scissors, Paper) | (Paper, Scissors)) => values._1 match
+          case Paper => Win
+          case _ => Loss
+        case _ => Draw
+
+    rps2.score + matchEnding.score
+
+  def playPart2(rps1: RPS, rps2: RPS): Int =
+    import PlayResult._
+    def matchEnding: PlayResult =
+      rps2 match
+        case Rock => Loss
+        case Paper => Draw
+        case Scissors => Win
+
+    val toPlay = (matchEnding, rps1) match
+      case (Draw, value) => value
+      case (Win, Rock) => Paper
+      case (Loss, Rock) => Scissors
+      case (Win, Paper) => Scissors
+      case (Loss, Paper) => Rock
+      case (Win, Scissors) => Rock
+      case (Loss, Scissors) => Paper
+
+    toPlay.score + matchEnding.score
+
+def calc(inputLine: String): (Int, Int) =
+  (calcPart1(inputLine), calcPart2(inputLine))
+
+def calcPart1(inputLine: String): Int =
+  inputLine match
+    case s"$first $second" => RPS.playPart1(RPS.fromChar(first.head), RPS.fromChar(second.head))
+
+def calcPart2(inputLine: String): Int =
+  inputLine match
+    case s"$first $second" => RPS.playPart2(RPS.fromChar(first.head), RPS.fromChar(second.head))
--- 2-12/src/test/scala/MySuite.scala
+++ 2-12/src/test/scala/MySuite.scala
@@ -1,9 +1,12 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
-  }
-}
+class MySuite extends munit.FunSuite:
+  test("main test on final data"):
+    val (score1, score2) = Solver.solve
+    assertEquals(score1, "13526")
+    assertEquals(score2, "14204")
+
+  test("main test on initial data"):
+    val (score1, score2) = Solver.solveTest
+    assertEquals(score1, "15")
+    assertEquals(score2, "12")
